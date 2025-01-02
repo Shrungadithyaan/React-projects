5869b03fb09649360da7ed30834f38f64a17fcc0
shrungadithyaan <shrungadithyaan@gmail.com>
2025-01-03 05:12:19 +0530

    React Essentials added

--- 03-React-Essentials/01-starting-project/src/App.jsx
+++ 03-React-Essentials/01-starting-project/src/App.jsx
@@ -1,44 +1,77 @@
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import { CORE_CONCEPTS  } from './data.js';
-import TabButton from './components/TabButton.jsx';
+import { useState } from "react";
+
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  let tabContant = "please click a button";
-  function handleSelect(selectedButton){
-    // selectedButton => 'components','jsx','props','state'
-    tabContant = selectedButton;
-    console.log(tabContant);
-}
+  const [selectedTopic, setSelectedTopic] = useState();
 
-console.log("app component executing");
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
+  }
+
+  console.log("APP COMPONENT EXECUTING");
+
+  let tabContent = <p>Please select the topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
-      
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
+          {/* Dynamic topics fetching */}
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
-        <section id='examples'>
-          <h2>Example</h2>
+        <section id="examples">
+          <h2>Examples</h2>
           <menu>
-              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-              {/* <TabButton label="Components"></TabButton> */}
-              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
-          {tabContant}
+          {tabContent}
         </section>
-        <h2>Time to get started!</h2>
       </main>
     </div>
   );
--- 03-React-Essentials/01-starting-project/src/components/TabButton.jsx
+++ 03-React-Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,10 @@
-export default function TabButton({ children , onSelect}) {
-  console.log("TabButton component executing")
+export default function TabButton({ children, onSelect, isSelected }) {
+  console.log("TabButton component executing");
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
